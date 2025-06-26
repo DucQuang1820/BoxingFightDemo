f0d94096267e1b4af789912eff75307f94d2a250
RyanDev <ryan@oneunivrs.vn>
2025-06-26 16:32:31 +0700

    fix bounce when collide

--- DucQuang_IB-Fight-Scene/Assets/Scripts/PlayerController.cs
+++ DucQuang_IB-Fight-Scene/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class MovementController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public FixedJoystick joystick;  
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    public FixedJoystick joystick; 
+    public Animator animator;
     public Transform player;
     public Transform cam;
     public Vector3 offset; 
     public Rigidbody rb;
     
-    private float smoothSpeed = 0.125f;
+    private float smoothSpeed = 1f;
     private float moveSpeed = 2f;
-    private float rotateSpeed = 5f;
-    private Vector3 velocity = Vector3.zero;
+    private float rotateSpeed = 10f;
 
     void Start()
     {
         
     }
 
-    private void Update()
-    {
-        if (Gamepad.current != null)
-        {
-            Gamepad.current.leftStick.ReadValue();
-        }
-    }
-
     private void FixedUpdate()
     {
         var direction = new Vector3(joystick.Horizontal, 0f, joystick.Vertical);
 
-        if (direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.01f)
         {
             Vector3 move = direction.normalized * moveSpeed * Time.fixedDeltaTime;
             rb.MovePosition(rb.position + move);
@@ -39,12 +32,13 @@ public class MovementController : MonoBehaviour
             Quaternion yRotation = Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f);
             transform.rotation = Quaternion.Slerp(transform.rotation, yRotation, rotateSpeed * Time.fixedDeltaTime);
         }
+        animator.SetFloat(Speed, direction.magnitude);
     }
 
     private void LateUpdate()
     {
         var targetPosition = player.position + offset;
 
-        cam.position = Vector3.SmoothDamp(cam.position, targetPosition, ref velocity, smoothSpeed);
+        cam.position = Vector3.Lerp(cam.position, targetPosition, smoothSpeed);
     }
 }
